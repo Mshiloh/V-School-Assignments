b625b7ee091c0f922195c79a55f5b9cb34cc8523
systemoviaryride <mshiloh.code@gmail.com>
2018-03-02 10:56:07 -0700

    submitted if statments silver exercise

---
+++ exercises/if-statements/if-silver.js
@@ -0,0 +1,16 @@
+//Silver 1
+
+if (1 === "1") {
+    console.log("strict");
+    } else if (1 == "1") {
+        console.log("loose");
+    } else if (null) {
+    console.log("not equal at all");
+}
+
+
+//Silver 2
+
+if (1 <= 2 && 2 === 4) {
+    console.log("yes");
+    }
