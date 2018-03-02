705e9e0dcd6960c64d80b3c422ecb95418005f29
systemoviaryride <mshiloh.code@gmail.com>
2018-03-01 22:22:50 -0700

    submitted if statments preliminaries file

---
+++ exercises/if-statements/if-preliminaries.js
@@ -0,0 +1,26 @@
+//Preliminaries 1
+
+if (5 > 3) {
+    //do this code
+    console.log("is greater than");
+}
+
+
+//Preliminaries 2
+
+var cat = 3;
+
+if (cat === 3) {
+    console.log("is the length");
+}
+
+
+//Preliminaries 3
+
+var cat = true;
+
+var dog = false;
+
+if (cat != dog) {
+    console.log("not the same");
+}
